04652c60539113faffe5df608595cdecd7e61bf2
Faiq <faiq.kaboel@gmail.com>
2020-04-18 05:05:24 +0700

    setup server and database conneection

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const App = require('express')()
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+const db = require('./config/db')
+const port = process.env.PORT || 4400
+
+App.use(cors())
+App.use(morgan('combined'))
+App.use(bodyParser.json())
+App.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect(db.dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+    console.log('Connected to database...\n.\n\.\n.')
+
+    try {
+      App.listen(port, () => {
+        console.log(`Server started on port: ${port}...`)
+      })
+    } catch (err) {
+      console.log(`Server failed to start. | ${err.message}`)
+    }
+  }).catch(err => {
+    console.log(`Cannot connect to database. | ${err.message}`)
+  })
---
+++ src/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+  dbUri: `mongodb+srv://beslisser:dcHBgQSfnlE9gA0W@kluster-0-co84f.mongodb.net/beslisser?retryWrites=true&w=majority`
+}
