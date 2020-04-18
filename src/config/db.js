643c37a3911c03d2602c2d16d21f963129497218
Faiq <faiq.kaboel@gmail.com>
2020-04-19 00:23:52 +0700

    rebase project

--- src/config/db.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  dbUri: `mongodb+srv://beslisser:dcHBgQSfnlE9gA0W@kluster-0-co84f.mongodb.net/beslisser?retryWrites=true&w=majority`
-}
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-const User = require('./controller/UserController');
-
-module.exports = (App) => {
-
-  App.post('/user/register', User.register)
-  App.post('/user/login', User.login)
-  App.get('/user/detail', User.getUserDetail)
-
-}
